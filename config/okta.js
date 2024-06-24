687a5c8620a795d5f79e79d91c0cfb8bb55f5684
aditya chaturvedi <115239434+humancoder23@users.noreply.github.com>
2024-06-24 13:26:12 +0530

    config file

---
+++ config/okta.js
@@ -0,0 +1,6 @@
+module.exports = {
+  metadataUrl: 'https://dev-55143921.okta.com/app/exkhc3qf38IMpkefw5d7/sso/saml/metadata',
+  entryPoint: 'https://dev-55143921.okta.com/app/dev-55143921_angularokta_1/exkhc3qf38IMpkefw5d7/sso/saml',
+  issuer: 'http://www.okta.com/exkhc3qf38IMpkefw5d7',
+  privateKey: '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----'  // Optional
+};
